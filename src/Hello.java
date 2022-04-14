29a9465b7f1e4bf2973a17e477dc873a6568f6ee
DWT527 <908445278@qq.com>
2022-04-14 17:20:41 +0800

    1111

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ import com.sun.jdi.PathSearchingVirtualMachine;
 public class Hello {
     public static void main(String[] args) {
         System.out.println(2222);
+        System.out.println(2222);
     }
 }
